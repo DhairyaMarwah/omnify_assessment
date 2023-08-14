822e8e90ed9a136da7c69776689157eeb981e1c2
dhairyamarwah <dhairyamarwah01@gmail.com>
2023-08-15 05:21:46 +0530

    bug fixed

--- client/src/assets/Icons/index.js
+++ client/src/assets/Icons/index.js
@@ -1,7 +1,6 @@
 import Logo from "./Logo.svg";
 import Home from "./Home.svg";
-import Logout from "./Logout.svg";
-import Favourite from "./Favourite.svg";
+import Logout from "./Logout.svg"; 
 import Fav from "./Fav.svg";
 import Search from "./Search.svg";
 import Bg from "./bg.png";
@@ -11,8 +10,7 @@ import EmptyHeart from "./EmptyHeart.svg";
 const Icons = {
   Logo,
   Home,
-  Logout,
-  Favourite,
+  Logout, 
   Search,
   Bg,
   Card,
