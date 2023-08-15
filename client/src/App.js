628ffb2add9b04f4c773bd6bf808a66c1315ddd7
dhairyamarwah <dhairyamarwah01@gmail.com>
2023-08-15 05:47:21 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter ,HashRouter} from "react-router-dom";
+import {  HashRouter} from "react-router-dom";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
